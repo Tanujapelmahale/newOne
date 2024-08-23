903c2e25eca22efef488f1057de9f402aecfae8e
Tanujapelmahale <tanujapelmahale@gmail.com>
2024-08-23 23:52:02 +0530

    changes text

--- first.java
+++ first.java
@@ -3,5 +3,9 @@ class first
 {
     public static void main(String[] args) {
         System.out.println("Can i become a expert in java");
+        int a=2;
+        String name="neha";
+        System.out.println("The "+name+ "win by" +a+ "time");
+        
     }
 }
